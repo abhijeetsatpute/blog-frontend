61c3a39f492365935aca89b9dd603c65262dfe3c
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-25 17:51:55 +0530

    412. Synchronizing POST Additions:
    + Listening to post event with .on()

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -41,7 +41,13 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080');
+    const socket = openSocket('http://localhost:8080');
+    socket.on('post', data => {
+      console.log('Lets changes');
+      if(data.action == 'create'){
+        this.addPost(data.post)
+      }
+    })
   }
 
   addPost = post => {
